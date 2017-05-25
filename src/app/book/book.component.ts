c5f3b48fa72c1d8bd1186c48264de6d2b911acc6
Sunil <sunil10790@gmail.com>
2017-05-25 09:04:14 +0530

    Add single book

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -3,11 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.css']
+  styleUrls: ['./book.component.css'],
+  host: {
+    class: 'row ui grid posts'
+  }
 })
 export class BookComponent implements OnInit {
+  votes: number;
+  title: string;
+  link: string;
 
-  constructor() { }
+  constructor() {
+    this.title = 'Eloquent JavaScript';
+    this.link = 'http://eloquentjavascript.net/';
+    this.votes = 37;
+  }
+
+  voteUp(): boolean{
+    this.votes  += 1;
+    return false;
+  }
+
+  voteDown(): boolean{
+    this.votes  -= 1;
+    return false;
+  }
 
   ngOnInit() {
   }
